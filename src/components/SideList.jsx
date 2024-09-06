90616a69d23faaa2d7ea907d5c4c239851136da1
Sina Abedi <Sinaabediofficial@gmail.com>
2024-09-06 23:58:36 +0330

    add option

--- src/components/SideList.jsx
+++ src/components/SideList.jsx
@@ -1,11 +1,10 @@
 function SideList({ data: { image, title } }) {
   return (
-    <div >
+    <div>
       <img src={image} alt={title} />
       <p>{title}</p>
     </div>
   );
 }
-
 
 export default SideList;
