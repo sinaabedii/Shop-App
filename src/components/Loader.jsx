ce8570b97b58eabf5de5326c20410e205b03d6b0
sina-abedi <110241278+sinaabedii@users.noreply.github.com>
2024-10-05 18:23:14 +0330

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { RotatingLines } from "react-loader-spinner";
 function Loader() {
   return (
     <div
-      className="mx-auto justify-center items-center text-center "
+      className="mx-auto grid justify-center items-center text-center "
       style={{ width: "100%", marginTop: "100px", height: "1000px" }}
     >
       <RotatingLines
